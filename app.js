250c38c2456ca5be93d88e9568c4f144b6cbab6a
kierepkae <kierepkaedyta@gmail.com>
2019-10-25 10:30:55 +0100

    add sample questions to app.js

--- app.js
+++ app.js
@@ -2,3 +2,30 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => console.log('listening on port 3000'));
+
+let goodAnswers = 0;
+let callToAFriendUsed = false;
+let askTheCrowdUsed = false;
+let reducedToHalfUsed = false;
+
+const questions = [
+  {
+    question: 'What is the best programming language?',
+    answers: ['Swift', 'Kotlin', 'C++', 'Ruby'],
+    correctAnswer: 3
+  },
+  {
+    question: 'What is OOP?',
+    answers: ['cloth brand', 'restaurant', 'paradigm', 'new BMW model'],
+    correctAnswer: 2
+  },
+  {
+    question: 'What\s the best way to learn programming?',
+    answers: ['group work', 'writing code', 'copy + paste code', 'watching YouTube tutorials'],
+    correctAnswer: 1
+  }
+]
+
+app.get('/question', (req, res) => {
+
+})
